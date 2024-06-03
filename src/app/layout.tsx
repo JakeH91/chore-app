854be817a1d44a867af861892fb4bd96c58c292b
JakeH91 <jakereh91@gmail.com>
2024-06-03 14:30:51 +0200

    feat(nav): add some basic navigation

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,9 @@
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
-import { UserProvider, useUser } from '@auth0/nextjs-auth0/client';
+import { UserProvider } from '@auth0/nextjs-auth0/client';
 import './globals.css';
 import Profile from './components/organisms/Profile';
+import Link from 'next/link';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -19,9 +20,17 @@ export default function RootLayout({
   return (
     <html lang="en">
       <UserProvider>
-        <body className={inter.className}>
+        <body className={`${inter.className} flex flex-row h-screen`}>
           <Profile />
-          {children}
+          <aside className="flex flex-col w-60 border-r border-black h-full">
+            <Link className="p-4 w-full" href="/">
+              Home
+            </Link>
+            <Link className="p-4 w-full" href="/tasks">
+              Chores
+            </Link>
+          </aside>
+          <main className="p-8">{children}</main>
         </body>
       </UserProvider>
     </html>
